f877f4a619c66bd3857d8def3a11878947edef1b
cfig <yuyezhong@gmail.com>
2020-01-14 12:36:19 +0800

    update template

--- template_for_vim/=template=.cmake
+++ template_for_vim/=template=.cmake
@@ -1,13 +1,35 @@
-cmake_minimum_required (VERSION 2.8)
+cmake_minimum_required (VERSION 3.10)
 
 # projectname is the same as the main-executable
 project(%HERE%%FDIR%)
 
+# def
 add_definitions('-g')
 add_definitions('-Wall')
 #add_definitions('-std=c++11')
 
+# compile exe
 add_executable(${PROJECT_NAME} ${PROJECT_NAME}.cpp)
 
-add_custom_target(${PROJECT_NAME}-symlink ALL ln --force -s ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME} ${CMAKE_SOURCE_DIR}/${PROJECT_NAME} DEPENDS ${PROJECT_NAME})
-set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${CMAKE_SOURCE_DIR}/${PROJECT_NAME})
+# link .so
+find_library(log liblog.so)
+target_link_libraries(hello log)
+
+
+# for YCM
+SET(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+IF( EXISTS "${CMAKE_CURRENT_BINARY_DIR}/compile_commands.json" )
+  EXECUTE_PROCESS( COMMAND ${CMAKE_COMMAND} -E create_symlink
+    ${CMAKE_CURRENT_BINARY_DIR}/compile_commands.json
+    ${CMAKE_CURRENT_SOURCE_DIR}/compile_commands.json
+  )
+ENDIF()
+
+# for Android NDK: MUST be placed before project()
+# $NDK is needed: eg. NDK=/home/yu/.programs/AndroidSdk/ndk-bundle
+SET(CMAKE_TOOLCHAIN_FILE $ENV{NDK}/build/cmake/android.toolchain.cmake)
+SET(ANDROID_ABI armeabi-v7a)
+SET(ANDROID_NATIVE_API_LEVEL 26)
+message("ABI ${ANDROID_ABI}")
+message("CMAKE_TOOLCHAIN_FILE  is ${CMAKE_TOOLCHAIN_FILE}")
